c94b2a5675b70831f8330d1c483e2fe3b9da2b8f
Nikhil <1nikhilnedunuri1@gmail.com>
2012-09-05 22:47:02 -0500

    This is commited to test whether a new branch is been created or not , and this file do exist in that branch after a push

---
+++ RackersToDo/src/com/racker/todo/beans/TestBean.java
@@ -0,0 +1,17 @@
+package com.racker.todo.beans;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: NikNed's
+ * Date: 9/5/12
+ * Time: 10:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestBean {
+
+
+    public static void main(String args[])
+    {
+        System.out.println("Just a small test to work on ");
+    }
+}
