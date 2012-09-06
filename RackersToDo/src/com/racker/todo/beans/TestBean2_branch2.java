56da0a68d1f05eddd643c37a540970d15c2ad896
Nikhil <1nikhilnedunuri1@gmail.com>
2012-09-05 22:54:47 -0500

    This is commited to test whether a new branch 2 is been created or not , and this file do exist in that branch after a push

---
+++ RackersToDo/src/com/racker/todo/beans/TestBean2_branch2.java
@@ -0,0 +1,17 @@
+package com.racker.todo.beans;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: NikNed's
+ * Date: 9/5/12
+ * Time: 10:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestBean2_branch2 {
+
+
+    public static void main(String args[])
+    {
+        System.out.println("This is a second branch file");
+    }
+}
