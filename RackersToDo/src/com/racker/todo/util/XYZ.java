729139d4c658fc0dd911d8d48d78c67679cfdae8
Nikhil <1nikhilnedunuri1@gmail.com>
2012-09-19 00:57:06 -0500

    added new class file

---
+++ RackersToDo/src/com/racker/todo/util/XYZ.java
@@ -0,0 +1,11 @@
+package com.racker.todo.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: NikNed's
+ * Date: 9/19/12
+ * Time: 12:54 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class XYZ {
+}
